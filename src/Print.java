fd11a8381d0bebcc61abe7867e24d3ab6a5f0ab8
Netsan <89118473+netsan1@users.noreply.github.com>
2021-10-25 08:08:43 +0530

    Create Print.java

---
+++ src/Print.java
@@ -0,0 +1,13 @@
+public class Print {
+    public static void main(String[] args) {
+        final int one = 'A'/'A';
+
+        final String ten = ".........."; // 10 
+        
+        final int num = (ten.length() * ten.length());
+
+        for (int i=one; i<=(num); i++) {
+            System.out.println(i);
+        }  
+    }
+}
